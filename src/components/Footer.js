812eb0f3a1d2ec943f773db916cfff2fec12301c
Vadim Lipatov <lipatovvs73@gmail.com>
2022-11-05 20:56:06 +0600

    fix year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="page-footer">
       <div className="footer-copyright">
         <div className="container">
-          © {"{"}new Date().getFullYear(){"}"} Copyright Text
+          © {new Date().getFullYear()} Copyright Text
           <a
             className="grey-text text-lighten-4 right"
             href="https://github.com/vadimlipatov/react-shop"
